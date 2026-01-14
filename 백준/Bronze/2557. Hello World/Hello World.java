647dc04a2511edb6e6fd0e55bcae6dcd34732641
WJH <55099632+jhwon91@users.noreply.github.com>
2026-01-14 22:57:54 +0900

    [Bronze V] Title: Hello World, Time: 100 ms, Memory: 14020 KB -BaekjoonHub

--- 백준/Bronze/2557. Hello World/Hello World.java
+++ 백준/Bronze/2557. Hello World/Hello World.java
@@ -1,6 +1,8 @@
+
 public class Main {
-    public static void main(String args[]){
-        System.out.print("Hello World!");
+    public static void main(String[] args){
+        StringBuilder sb = new StringBuilder();
+        sb.append("Hello World!");
+        System.out.println(sb);
     }
 }
-
