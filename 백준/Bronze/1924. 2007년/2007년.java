db9e76977a7db56af648f1ad82986b00f8e548ae
WJH <55099632+jhwon91@users.noreply.github.com>
2026-01-14 23:05:22 +0900

    [Bronze I] Title: 2007년, Time: 100 ms, Memory: 14128 KB -BaekjoonHub

--- 백준/Bronze/1924. 2007년/2007년.java
+++ 백준/Bronze/1924. 2007년/2007년.java
@@ -1,18 +1,26 @@
-import java.io.*;
-import java.util.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int month = Integer.parseInt(st.nextToken());
-        int day = Integer.parseInt(st.nextToken());
-        int[] days = {0,31,28,31,30,31,30,31,31,30,31,30,31};
-        String[] weeks = {"SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"};
-        int result = 0;
-        for(int i = 0; i<month;i++){
-            result += days[i];
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        int x = Integer.parseInt(st.nextToken());
+        int y = Integer.parseInt(st.nextToken());
+
+        int[] lastDay = {0,31,28,31,30,31,30,31,31,30,31,30,31};
+        String[] week = {"SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"};
+        int sumDay = 0;
+
+        for (int i = 0; i < x; i++) {
+            sumDay += lastDay[i];
         }
-        System.out.println(weeks[(day + result) % 7]);
+
+        sumDay += y;
+        System.out.println(week[sumDay%7]);
+
     }
 }
